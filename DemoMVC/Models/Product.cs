db13720c0cd58bf941be723cb6fe60f4bcfd0c97
“tranduythanhlam157@gmail.com” <tranduythanhlam157@gmail.com>
2026-03-10 13:02:35 +0700

    upload DemoMVC project

---
+++ DemoMVC/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace DemoMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public double Price { get; set; }
+    }
+}
